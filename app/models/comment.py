502b1e46817216dae3cfba9b7114ce9d9fbc506e
Eleanor Billerbeck <ellie.billerbeck@gmail.com>
2022-10-22 14:45:26 -0500

    comment-model

--- app/models/comment.py
+++ app/models/comment.py
@@ -1,26 +1,25 @@
 from .db import db
 import datetime
 
-class Answer(db.Model):
-    __tablename__ = 'answers'
+class Comment(db.Model):
+    __tablename__ = 'comments'
 
     id = db.Column(db.Integer, primary_key=True)
     userId = db.Column(db.Integer, db.ForeignKey('users.id'))
-    questionId = db.Column(db.Integer, db.ForeignKey('questions.id'))
+    answerId = db.Column(db.Integer, db.ForeignKey('answers.id'))
     body = db.Column(db.String(2000), nullable=False)
     created_on = db.Column(db.Date, default=datetime.datetime.utcnow)
     updated_on = db.Column(db.Date, onupdate=datetime.datetime.utcnow)
 
-    user = db.relationship('User', back_populates='answers')
-    question = db.relationship('Question', back_populates='answers')
-    comments = db.relationship('Comment', back_populates='answer')
+    user = db.relationship('User', back_populates='comments')
+    answer = db.relationship('Answer', back_populates='comments')
 
 
     def to_dict(self):
         return {
             'id': self.id,
             'userId': self.userId,
-            'questionId': self.questionId
+            'answerId': self.answerId
             'body': self.body
             'created_on': self.created_on
             'updated_on': self.updated_on
--- app/models/user.py
+++ app/models/user.py
@@ -13,6 +13,7 @@ class User(db.Model, UserMixin):
 
     questions = db.relationship('Question', back_populates='user')
     answers = db.relationship('Answer', back_populates='user')
+    comments = db.relationship('Comment', back_populates='user')
 
     @property
     def password(self):
