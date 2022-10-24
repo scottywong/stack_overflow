11630a22603bdd1cdd641d5c8dd947271e0650f2
scottwong <wong.sctt@gmail.com>
2022-10-23 18:08:56 -0700

    start Answer Component

---
+++ react-app/src/components/Answer/AnswerListItem/index.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import {AnswerListItem} from './AnswerListItem';
+
+function AnswerListItem() {
+
+}
+
+export default AnswerListItem;
---
+++ react-app/src/components/Profile.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+function MyProfilePage() {
+
+    
+    return (
+
+    )
+
+}
