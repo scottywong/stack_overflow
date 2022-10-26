535205930b3e1b76f96b51e6f6ef86aede21bf66
Eleanor Billerbeck <ellie.billerbeck@gmail.com>
2022-10-25 19:37:55 -0500

    answer routes updated

--- app/forms/comment_form.py
+++ app/forms/comment_form.py
@@ -1,8 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import TextAreaField, IntegerField, SubmitField
 from wtforms.validators import DataRequired
+from app.models import Answer
+
+# Check that answer exists
+def valid_answer(form, field):
+    answerId = field.data
+    answer = Answer.query.get(answerId)
+    if answer is None:
+        raise ValidationError('Answer not found')
 
 class CommentForm(FlaskForm):
-    answerId = IntegerField('answerId', validators=DataRequired())
+    answerId = IntegerField('answerId', validators=[DataRequired(), valid_answer])
     body = TextAreaField('Comment', validators=[DataRequired()])
-    submit = SubmitField('Submit')
