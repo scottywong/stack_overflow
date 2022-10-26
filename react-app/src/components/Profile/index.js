66cfd3d7fe3c46d22faa77ed1275eca9a09e244e
Keith Glines <keithglines@yahoo.com>
2022-10-26 19:10:19 -0400

    Profile update

--- react-app/src/components/Profile/index.js
+++ react-app/src/components/Profile/index.js
@@ -10,7 +10,7 @@ const Profile = () => {
         <div className='profile-Container'>
             
             <MyQuestionList/>
-            {/* <MyAnswerList/> */}
+            <MyAnswerList/>
 
         </div>
 
