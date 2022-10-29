5f1c5b3a2f18a5f7cefd502169404b22629e7c7c
isaacd <isaacdarzi@gmail.com>
2022-10-29 15:01:57 -0400

    added demo and vote backend changes

---
+++ react-app/src/components/auth/DemoLogin.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import { login } from '../../store/session';
+
+const DemoLogin = ({ setShowModal }) => {
+  const [errors, setErrors] = useState([]);
+  const user = useSelector(state => state.session.user);
+  const dispatch = useDispatch();
+
+  if (user) {
+    return <Redirect to='/home' />;
+  }
+
+  const onLogin = async (e) => {
+    e.preventDefault();
+    const data = await dispatch(login('demo@aa.io', 'password'));
+    if (data) {
+      setErrors(data);
+    }
+  };
+
+
+
+  return (
+    <form onSubmit={onLogin}>
+      <ul>
+       
+        {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+      
+      </ul>
+      
+      <button type="submit">Log In As Demo User</button>
+ 
+    </form>
+  );
+};
+
+export default DemoLogin;
