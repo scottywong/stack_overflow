6a802b60b45969d9c82c76f58d96768c686d4233
scottwong <wong.sctt@gmail.com>
2022-10-31 19:56:24 -0700

    fix nav alignment

--- react-app/src/components/Questions/QuestionListItem/index.js
+++ react-app/src/components/Questions/QuestionListItem/index.js
@@ -5,7 +5,7 @@ import './QuestionListItem.css';
 function QuestionListItem({ question }) {
 
   let questionTitle;
-  question?.body && question?.body.length >= 50 ? questionTitle = question.body.substring(0,50) + ' ...' : questionTitle = question.body
+  question?.title && question?.title.length >= 50 ? questionTitle = question.title.substring(0,50) + ' ...' : questionTitle = question.title
   return (
     <div className='qli-container'>
       <NavLink className="qli-link" to={`/questions/${question.id}`}>
