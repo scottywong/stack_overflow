011b564621682aefb8cb274d5cd636db9955e3be
scottwong <wong.sctt@gmail.com>
2022-11-02 20:53:29 -0700

    remove vote from reducer

--- react-app/src/components/Answers/AnswerListItem/index.js
+++ react-app/src/components/Answers/AnswerListItem/index.js
@@ -6,17 +6,20 @@ import './AnswerListItem.css';
 function AnswerListItem({answer}) {
 
     let answerBody;
-    answer?.body && answer?.body?.length >= 50 ? answerBody = answer?.body?.substring(0,50) + ' ...' : answerBody = answer?.body
-    return (
-        <div className='ali-container'>
-        <NavLink className="ali-link" to={`/questions/${answer?.questionId}`}>
-            <h3 className='ali-title'>{answerBody}</h3>
-        </NavLink>
+    answer?.body && answer?.body?.length >= 50 ? answerBody = answer?.body?.substring(0,50) + ' ...' : answerBody = answer?.body;
+    return (<div>
 
-        <p>Answered by: {answer?.username}</p>
-  
-        </div>
-    ); 
+            {console.log('this is the answer: ', answer)}
+            {answer && (
+                <div className='ali-container'>
+                <NavLink className="ali-link" to={`/questions/${answer.questionId}`}>
+                    <h3 className='ali-title'>{answerBody}</h3>
+                </NavLink>
+                <p>Answered by: {answer.username}</p>
+                </div>
+            )}
+            </div>
+    )
 }
 
 export default AnswerListItem;
